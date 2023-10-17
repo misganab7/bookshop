5523bc25072154a580c44ccc47f5929421dea767
misganab7 <132500099+misganab7@users.noreply.github.com>
2023-10-17 11:21:50 +0300

    Add files via upload

---
+++ project/prac.php/prac.php
@@ -0,0 +1,18 @@
+<?php
+$name='misgana';
+$age=21;
+
+echo $name . $age;
+echo $name[1];
+
+$num =['num1', 'num2', 'num3'];
+print_r($num);
+
+$sec =
+[
+    'name'=>'misgana',
+    'age'=>'21',
+    'education' =>'college',
+    'field' =>'computer science'
+]
+?>
